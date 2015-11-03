8f61a598b31586e15dec3af3d2c6f064e0a81b41
Ilya Kowalewski <ilya.kowalewski@gmail.com>
2015-11-03 23:03:06 +0200

    Working & tested prototype

---
+++ application.go
@@ -0,0 +1,127 @@
+package climax
+
+import (
+	"fmt"
+	"os"
+)
+
+// Application is a main CLI instance.
+//
+// By default, Climax provides its own implementation of version
+// command, but it will use "version" command instead if you
+// provide one.
+type Application struct {
+	Name    string // `go`
+	Brief   string // `Go is a tool for managing Go source code.`
+	Version string // `1.5`
+
+	Commands []Command
+	Topics   []Topic
+}
+
+func (a *Application) commandByName(name string) *Command {
+	for i, command := range a.Commands {
+		if command.Name == name {
+			return &a.Commands[i]
+		}
+	}
+
+	return nil
+}
+
+func (a *Application) topicByName(name string) *Topic {
+	for i, topic := range a.Topics {
+		if topic.Name == name {
+			return &a.Topics[i]
+		}
+	}
+
+	return nil
+}
+
+func (a Application) isNameAvailable(name string) bool {
+	hypo, jypo := a.commandByName(name), a.topicByName(name)
+	if hypo != nil || jypo != nil {
+		return false
+	}
+
+	return true
+}
+
+// AddCommand does literally what its name says.
+func (a *Application) AddCommand(command Command) {
+	a.Commands = append(a.Commands, command)
+}
+
+// AddTopic does literally what its name says.
+func (a *Application) AddTopic(topic Topic) {
+	a.Topics = append(a.Topics, topic)
+}
+
+// Run executes a CLI.
+//
+// Take a note, Run panics if len(os.Args) < 1
+func (a Application) Run() int {
+	if len(os.Args) < 1 {
+		panic("shell-provided arguments are not present")
+	}
+	arguments := os.Args[1:]
+	// $ program
+	//           ^ no args
+	if len(arguments) == 0 {
+		println(a.globalHelp())
+		return 0
+	}
+
+	yankeeGoHome := func(errMsg string) {
+		printerr(fmt.Errorf("%s: %s", a.Name, errMsg))
+		os.Exit(1)
+	}
+
+	subcommandName := arguments[0]
+	subcommand := a.commandByName(subcommandName)
+
+	if subcommandName == "help" {
+		// $ program help
+		//           ^ one argument
+		if len(arguments) <= 1 {
+			println(a.globalHelp())
+			return 0
+		}
+
+		command := a.commandByName(arguments[1])
+		if command != nil {
+			println(a.commandHelp(command))
+			return 0
+		}
+
+		topic := a.topicByName(arguments[1])
+		if topic != nil {
+			println(topic.Text)
+			return 0
+		}
+
+		yankeeGoHome("no such command or help topic")
+	}
+
+	if subcommandName == "version" {
+		if subcommand != nil {
+			return subcommand.Run(Context{})
+		}
+
+		printf("%s version %s\n", a.Name, a.Version)
+		return 0
+	}
+
+	if subcommand != nil {
+		context, err := parseContext(subcommand.Flags, arguments[1:])
+		if err != nil {
+			yankeeGoHome(err.Error())
+		}
+
+		return subcommand.Run(*context)
+	}
+
+	yankeeGoHome("unknown subcommand \"" + subcommandName + "\"\n")
+	return 1
+}
---
+++ help.go
@@ -0,0 +1,114 @@
+package climax
+
+import (
+	"bytes"
+	"strings"
+	"text/template"
+)
+
+const globalHelpTemplate string = `{{.Brief}}
+
+Usage:
+
+	{{.Name}} {{if .Commands}}command [arguments]{{end}}
+
+{{if .Commands}}The commands are:
+{{range .Commands}}
+	{{.Name | printf "%-11s"}} {{.Brief}}{{end}}
+
+Use "{{.Name}} help [command]" for more information about a command.{{end}}
+{{if .Topics}}
+Additional help topics:
+{{range .Topics}}
+	{{.Name | printf "%-11s"}} {{.Brief}}{{end}}
+
+Use "{{.Name}} help [topic]" for more information about a topic.
+{{end}}`
+
+const commandHelpTemplate string = `Usage: {{commandUsage .Command}}
+
+{{.Help}}
+{{if .Flags}}
+Available options:
+{{range .Flags}}
+	{{flagUsage .}}
+		{{.Help | tabout}}{{end}}{{end}}
+{{if .Examples}}
+Examples:
+{{$app := .App}}{{$cmd := .Name}}{{range .Examples}}
+	$ {{$app}} {{$cmd}} {{.Usecase}}
+		{{.Description | tabout}}
+{{end}}{{end}}`
+
+func templated(canvas string, data interface{}) string {
+	t := template.New("")
+	t.Funcs(template.FuncMap{
+		"tabout":       alignMultilineHelp,
+		"commandUsage": commandUsage,
+		"flagUsage":    flagUsage,
+	})
+	template.Must(t.Parse(canvas))
+
+	var b bytes.Buffer
+
+	err := t.Execute(&b, data)
+	if err != nil {
+		panic(err)
+	}
+
+	output := b.String()
+
+	// TODO: Fix this nasty workaround for templating ASAP!
+	output = strings.Replace(output, "\n\n\n", "", -1)
+
+	return output
+}
+
+func alignMultilineHelp(text string) string {
+	return strings.Replace(text, "\n", "\n\t\t", -1)
+}
+
+func commandUsage(command Command) string {
+	if command.Usage != "" {
+		return command.Name + " " + command.Usage
+	}
+
+	usage := command.Name
+	for _, flag := range command.Flags {
+		usage += " [" + flagUsage(flag) + "]"
+	}
+
+	return usage
+}
+
+func flagUsage(flag Flag) string {
+	var short string
+	if flag.Short != "" {
+		short = "-" + flag.Short + ", "
+	}
+
+	if flag.Usage != "" {
+		return short + flag.Usage
+	}
+
+	usage := "--" + flag.Name
+	if flag.Variable {
+		usage += "=\"\""
+	}
+
+	return short + usage
+}
+
+func (a *Application) globalHelp() string {
+	return templated(globalHelpTemplate, *a)
+}
+
+func (a *Application) commandHelp(command *Command) string {
+	return templated(commandHelpTemplate, struct {
+		Command
+		App string
+	}{
+		*command,
+		a.Name,
+	})
+}
